87240d2d2b5ca6f997d2249167b570cf1ed3ff73
Student <Student@laspositascollege.edu>
2026-02-23 21:33:45 -0800

    moving to home PC

--- part-a/part-a.cpp
+++ part-a/part-a.cpp
@@ -10,16 +10,30 @@ using namespace std;
 *******************************************************************************/
 
 template <typename T>
-T findMaxRecTail(const T[] arr, const int size, int = 0)
+T findMaxRecTail(const T arr[], const int size)
 {
+    if (size <= 0) throw std::invalid_argument("Array cannot be empty");
     // TO DO: Implement your code
-}
+    if (size == 1) return arr[0];
+    T max = findMaxRecTail(arr + 1, size - 1);  
+    return (arr[0] > max) ? arr[0] : max;
+} 
 
 template <typename T>
-T findMaxRecBinarySplit(const T[] arr, const int left, const int right)
+T findMaxRecBinarySplit(const T arr[], const int size)
 {
     // TO DO: Implement your code
+    if (size <= 0) throw std::invalid_argument("Array cannot be empty");
+    if (size == 1) return arr[0];
+
+    int midpoint = size / 2;
+
+    T maxLeft = findMaxRecBinarySplit(arr, midpoint);
+    T maxRight = findMaxRecBinarySplit(arr + midpoint, size - midpoint);
+
+    return (maxLeft > maxRight) ? maxLeft : maxRight;
 }
+
 /*******************************************************************************
  * Description:
  * Starting point of the program. Creates two arrays, one fixed and the other
@@ -43,11 +57,15 @@ int main() {
 
     // display the maximum
     cout << setfill('-') << setw(40) << "" << endl;
+    try {
     cout << "Maximum using Recursion: "
          << findMaxRecTail(myArray, SIZE) << endl 
          << "From Binary split:" 
-        << findMaxRecBinarySplit(myArray, 0, SIZE-1)
+        << findMaxRecBinarySplit(myArray, SIZE)
          << "Should be 130 for the fixed array\n";
+    } catch (std::invalid_argument& e) {
+        cerr << "Error" << e.what();
+    }
     cout << setfill('-') << setw(40) << "" << endl << endl;
 
     // create a random array
@@ -65,8 +83,12 @@ int main() {
          << *max_element(randArray, randArray + SIZE_2) << endl;
 
     // find the maximum using the recursive implementation
+    try {
     cout << "Maximum using Recursion: "
          << findMaxRecTail(randArray, SIZE_2) << endl;
+    } catch (std::invalid_argument& e) {
+        cerr << "Error" << e.what();
+    }
     cout << setfill('-') << setw(40) << "" << endl;
 
     // terminate
